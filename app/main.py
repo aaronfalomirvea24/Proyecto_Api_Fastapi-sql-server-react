818aae3aa6a8abd3d2386554737da6c2ca30e0e8
Aaron2025 <Aaronfalomirvea13@gmail.com>
2026-07-08 17:08:24 -0700

    Fix CORS for Railway production

--- app/main.py
+++ app/main.py
@@ -1,22 +1,31 @@
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
-from app import  database
-from app import models,endpoints
+from app import database
+from app import models, endpoints
 from fastapi.middleware.cors import CORSMiddleware
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 app = FastAPI(
-    swagger_ui_parameters = {"persistAuthorization": True}
+    swagger_ui_parameters={"persistAuthorization": True}
 )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://proyectoapifastapi-sql-server-react-production.up.railway.app",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#token endpoint para autenticación
+
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenido a la API del Restaurante Ficticio!"}
+    return {"message": "Bienvenido a la API del Restaurante!"}
+
 models.Base.metadata.create_all(bind=database.engine)
 app.include_router(endpoints.router)
